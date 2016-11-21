1c5a6b6833f813f21fdaa9fbe5a81e130e6fe580
Dyeimys Franco <dyeimys@hotmail.com>
2016-11-21 12:28:22 -0200

    Organizando Repositório

---
+++ quest/src/main/java/br/ufg/jatai/fsw/QuestApplication.java
@@ -0,0 +1,12 @@
+package br.ufg.jatai.fsw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestApplication.class, args);
+	}
+}
