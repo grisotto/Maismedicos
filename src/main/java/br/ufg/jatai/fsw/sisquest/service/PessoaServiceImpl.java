c1850ebbd6a8248bc987fc09200d2158ba7f317f
Dyeimys Franco <dyeimys@hotmail.com>
2016-12-01 14:47:36 -0200

    Organizando pacotes, criando Service

--- src/main/java/br/ufg/jatai/fsw/sisquest/service/PessoaServiceImpl.java
+++ src/main/java/br/ufg/jatai/fsw/sisquest/service/PessoaServiceImpl.java
@@ -3,21 +3,21 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package br.ufg.jatai.fsw.sisquest.repository;
+package br.ufg.jatai.fsw.sisquest.service;
 
 import br.ufg.jatai.fsw.sisquest.model.Pessoa;
+import br.ufg.jatai.fsw.sisquest.repository.PessoaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-
 
 public class PessoaServiceImpl implements PessoaService {
 
-    
     @Autowired
     private PessoaRepository pessoaRepository;
+
     @Override
     public void savePessoa(Pessoa pessoa) {
         pessoaRepository.findAll();
         pessoaRepository.save(pessoa);
     }
-    
+
 }
