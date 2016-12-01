4ccf70cbd6c6a92ec6b80cde00e6199d944725ae
thevilela <gabrielvilelabarra@gmail.com>
2016-12-01 17:30:24 -0200

    Criados os services AlunoService e QuestionarioService..

---
+++ src/main/java/br/ufg/jatai/fsw/sisquest/service/QuestionarioService.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.ufg.jatai.fsw.sisquest.service;
+
+import br.ufg.jatai.fsw.sisquest.model.Equipe;
+import br.ufg.jatai.fsw.sisquest.model.Questionario;
+
+/**
+ *
+ * @author dfranco
+ */
+public interface QuestionarioService extends AbstractService<Questionario, Integer> {
+    
+}
