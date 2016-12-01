b561c1257d4f4399f2d968114f1e3c8833b9bebe
thevilela <gabrielvilelabarra@gmail.com>
2016-12-01 15:38:12 -0200

    Criando repositórios

---
+++ src/main/java/br/ufg/jatai/fsw/sisquest/repository/EquipeRepository.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.ufg.jatai.fsw.sisquest.repository;
+
+import br.ufg.jatai.fsw.sisquest.model.Equipe;
+import br.ufg.jatai.fsw.sisquest.model.Pessoa;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ *
+ * @author dfranco
+ */
+public interface EquipeRepository extends JpaRepository<Equipe, Integer> {
+    
+}
