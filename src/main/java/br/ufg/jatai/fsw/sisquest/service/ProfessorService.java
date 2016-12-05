2e1b0ff4186c460010236ddceaf2c0c11a867d94
Dyeimys Franco <dyeimys@hotmail.com>
2016-12-05 00:48:41 -0200

    Criando Repository e service para professor

---
+++ src/main/java/br/ufg/jatai/fsw/sisquest/service/ProfessorService.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.ufg.jatai.fsw.sisquest.service;
+
+import br.ufg.jatai.fsw.sisquest.model.Aluno;
+import br.ufg.jatai.fsw.sisquest.model.Equipe;
+
+/**
+ *
+ * @author dfranco
+ */
+public interface ProfessorService extends AbstractService<Aluno, Integer> {
+    
+}
