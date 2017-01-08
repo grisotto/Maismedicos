9fe7271c1d650b15bcbb2d438b564f8de8c30d68
Gabriel Vilela Barra <thevilela@localhost.thevilela>
2017-01-08 16:41:10 -0200

    Adicionado método listarProfessores()

--- src/main/java/br/ufg/jatai/fsw/squest/facade/ProfessorFacade.java
+++ src/main/java/br/ufg/jatai/fsw/squest/facade/ProfessorFacade.java
@@ -3,14 +3,20 @@ package br.ufg.jatai.fsw.squest.facade;
 import br.ufg.jatai.fsw.squest.AutenticateUser;
 import br.ufg.jatai.fsw.squest.controller.AlunoController;
 import br.ufg.jatai.fsw.squest.controller.ProfessorController;
+import br.ufg.jatai.fsw.squest.domain.Professor;
 import br.ufg.jatai.fsw.squest.service.ProfessorService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 /**
  * Created by thevilela on 08/01/17.
  */
+
+@Component
 public class ProfessorFacade {
 
     @Autowired
@@ -21,4 +27,9 @@ public class ProfessorFacade {
     @Autowired
     private AutenticateUser autenticateUser;
 
+    public List<Professor> listarProfessores(){
+        return professorService.findAll();
+    }
+
+    
 }
