10291aff45702ad31335b20d85d858cb95087be6
Dyeimys Franco <dyeimys@hotmail.com>
2017-01-10 00:06:19 -0200

    Está acessando o controller e enviando os dados corretos Agora falta: - ORganizar retorno - Converter objeto e salvar ele - Depois mostrar lá na tela principal

--- src/main/java/br/ufg/jatai/fsw/squest/controller/QuestionarioController.java
+++ src/main/java/br/ufg/jatai/fsw/squest/controller/QuestionarioController.java
@@ -7,14 +7,15 @@ package br.ufg.jatai.fsw.squest.controller;
 
 import br.ufg.jatai.fsw.squest.controller.modelForm.QuestãoModel;
 import br.ufg.jatai.fsw.squest.domain.Questionario;
-import java.io.Serializable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.io.Serializable;
+
 /**
- *
  * @author dfranco
  */
 @Controller
@@ -24,12 +25,18 @@ public class QuestionarioController implements Serializable {
     private static Logger log = LoggerFactory.getLogger(QuestionarioController.class.getName());
 
 
-    public void inserirQuestão(String correta, QuestãoModel questaoModel){
-
-
+    @PostMapping("/addQuestao")
+    public String inserirQuestão(String correto, QuestãoModel questaoModel) {
+        log.info("CORRETA: " + correto);
+        log.info("Alternativa A: " + questaoModel.getAlternativaA().getDescricao());
+        log.info("Alternativa B: " + questaoModel.getAlternativaB().getDescricao());
+        log.info("Alternativa C: " + questaoModel.getAlternativaC().getDescricao());
+        log.info("Alternativa D: " + questaoModel.getAlternativaD().getDescricao());
+        log.info("Alternativa E: " + questaoModel.getAlternativaE().getDescricao());
+        return "redirect:/app/questionario/inserir";
     }
+
     /**
-     *
      * @return
      */
 
@@ -37,22 +44,20 @@ public class QuestionarioController implements Serializable {
     public String QuestoesEquipeInserir(QuestãoModel questionario) {
         return "/app/questionario/inserir";
     }
-    
+
     /**
-    *
-    * @return
-    */
-   @RequestMapping(value = "/responder")
-   public String QuestoesEquipeResponder(Questionario questionario) {
-       return "/app/questionario/responder";
-   }
-   
-   /**
-   *
-   * @return
-   */
-  @RequestMapping(value = "/pontuacao")
-  public String QuestoesEquipePontuacao(Questionario questionario) {
-      return "/app/questionario/pontuacao";
-  }
+     * @return
+     */
+    @RequestMapping(value = "/responder")
+    public String QuestoesEquipeResponder(Questionario questionario) {
+        return "/app/questionario/responder";
+    }
+
+    /**
+     * @return
+     */
+    @RequestMapping(value = "/pontuacao")
+    public String QuestoesEquipePontuacao(Questionario questionario) {
+        return "/app/questionario/pontuacao";
+    }
 }
