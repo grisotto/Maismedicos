f619057055362243efc163cc6c0e4f5879abf2d0
Dyeimys Franco <dyeimys@hotmail.com>
2017-01-12 21:58:49 -0200

    Criado metodo no Repositorio que lista apaenas tarefas do prrofessor

--- src/main/java/br/ufg/jatai/fsw/squest/repository/EquipeRepository.java
+++ src/main/java/br/ufg/jatai/fsw/squest/repository/EquipeRepository.java
@@ -7,11 +7,15 @@ package br.ufg.jatai.fsw.squest.repository;
 
 import br.ufg.jatai.fsw.squest.domain.Equipe;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 /**
  *
  * @author vilela
  */
 public interface EquipeRepository extends JpaRepository<Equipe, Integer> {
-    
+    @Query("select e from Equipe e where e.tarefa.turma.professor.id = :professorID")
+    public List<Equipe> equipesDoProfessor(Integer professorID);
 }
