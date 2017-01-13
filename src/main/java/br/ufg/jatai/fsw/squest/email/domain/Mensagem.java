7ff6d7dc35d067fa92b156203fdfd083223729ed
Gabriel Vilela Barra <thevilela@localhost.thevilela>
2017-01-13 20:18:24 -0200

    Mudanças na classe Mensagem

--- src/main/java/br/ufg/jatai/fsw/squest/email/domain/Mensagem.java
+++ src/main/java/br/ufg/jatai/fsw/squest/email/domain/Mensagem.java
@@ -68,5 +68,29 @@ public class Mensagem {
         this.anexos = anexos;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
 
+        Mensagem mensagem = (Mensagem) o;
+
+        if (!destinatarios.equals(mensagem.destinatarios)) return false;
+        if (!bcc.equals(mensagem.bcc)) return false;
+        if (!cc.equals(mensagem.cc)) return false;
+        if (anexos != null ? !anexos.equals(mensagem.anexos) : mensagem.anexos != null) return false;
+        if (!assunto.equals(mensagem.assunto)) return false;
+        return corpo.equals(mensagem.corpo);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = destinatarios.hashCode();
+        result = 31 * result + bcc.hashCode();
+        result = 31 * result + cc.hashCode();
+        result = 31 * result + (anexos != null ? anexos.hashCode() : 0);
+        result = 31 * result + assunto.hashCode();
+        result = 31 * result + corpo.hashCode();
+        return result;
+    }
 }
