ca335e1ce2f044d4f057925bbc9679cf9f6603a5
grisotto <grisotto18@gmail.com>
2017-01-23 06:45:08 -0200

    QuizRespondendo falta apenas 1 dado para terminar de criar o objeto

--- src/main/java/br/ufg/jatai/fsw/squest/controller/QuizController.java
+++ src/main/java/br/ufg/jatai/fsw/squest/controller/QuizController.java
@@ -16,6 +16,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Set;
@@ -40,6 +41,8 @@ public class QuizController {
 
     @Autowired
     private QuestaoService questaoService;
+
+    private static Logger log = LoggerFactory.getLogger(QuestionarioController.class.getName());
 
     @GetMapping
     public String home(final Quiz quiz, ModelMap model) {
@@ -75,14 +78,33 @@ public class QuizController {
     }
 
     @PostMapping("/{quizID}/responder/{questaoID}")
-    public void respostaQuestao(QuestaoQuiz questaoQuiz, Alternativa alternativa) {
+    public String respostaQuestao(QuestaoQuiz questaoQuiz, Alternativa alternativaSelecionada, final BindingResult bindingResult , final String alternativa,@PathVariable Integer quizID ) {
+
+
+        if (bindingResult.hasErrors()) {
+
+        }
+        log.info("Entrda de Dados no @Controller");
+        log.info("Equipe: " + autenticateUser.getEquipe());
+        log.info("Alternativa: " + alternativaSelecionada.getDescricao());
+        log.info("Alternativaa: " + alternativa);
+//        log.info("Alternativa: " + alternativa.getQuestao());
+//        log.info("Descrição: " + questaoQuiz.respondido());
+//        log.info("Alternativa A: " + questaoQuiz.getQuestao().getAlternativas()..getAlternativaA().getDescricao());
+//        log.info("Alternativa B: " + questaoModel.getAlternativaB().getDescricao());
+//        log.info("Alternativa C: " + questaoModel.getAlternativaC().getDescricao());
+//        log.info("Alternativa D: " + questaoModel.getAlternativaD().getDescricao());
+//        log.info("Alternativa E: " + questaoModel.getAlternativaE().getDescricao());
+//        log.info("CORRETA: " + correto);
+
         Equipe equipe = autenticateUser.getEquipe();//Requcuperando equipe
         RespotaQuestaoQuiz respotaQuestaoQuiz = new RespotaQuestaoQuiz();//Cria Obbjeto de reposta
-        respotaQuestaoQuiz.setAlternativa(alternativa);//Colocando a autenrativa
+        respotaQuestaoQuiz.setAlternativa(alternativaSelecionada);//Colocando a autenrativa
+        log.info("Cheguei: ");
         respotaQuestaoQuiz.setEquipe(equipe);//Colocando a equipe
         respotaQuestaoQuiz.setQuestao(questaoQuiz.getQuestao());
 
-        questaoQuiz.getRespotaQuestaoQuizs().add(respotaQuestaoQuiz);//Adicionando a resposta
+//        questaoQuiz.getRespotaQuestaoQuizs().add(respotaQuestaoQuiz);//Adicionando a resposta
 
         questaoQuiz.getEquipeResponderam().add(equipe);
 
@@ -90,15 +112,15 @@ public class QuizController {
 
 //        quizService.atualizar()
 
-
+        return "redirect:/app/quiz/"+ quizID +"/responder/";
 
 
     }
 
     @PreAuthorize("hasAuthority('GRUPO')")
     @GetMapping("/{quizID}/responder/{questaoID}")
-    public String responderQuestao(@PathVariable Integer questaoID, Model model) {
-
+    public String responderQuestao(@PathVariable Integer questaoID, @PathVariable Integer quizID, Model model) {
+        model.addAttribute("quiz", quizService.find(quizID));
         model.addAttribute("questao", questaoService.find(questaoID));
 
         return "app/quiz/responder";
